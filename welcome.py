46a258edf005eed954873191cc73825aa1cc0b57
Kerami Ozsoy <keramiozsoy@hotmail.com>
2018-01-22 14:51:05 +0300

    I used __name__

--- welcome.py
+++ welcome.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(__filename__)
+app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "welcome page!!"
 
-if __filename__ == "__main__":
+if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8888)
