5f1ecd2f234a0d4dfe3b6cf262de8192e5980b0e
Kerami Ozsoy <keramiozsoy@hotmail.com>
2018-01-29 10:29:02 +0300

    app.py - Now login and logout working

--- app.py
+++ app.py
@@ -1,27 +1,41 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session
+from flask import flash
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return render_template('home.html')
 
 @app.route("/login",methods=['POST'])
 def login():
     if  request.form['username'] == 'test' and request.form['password'] == 'test' :
-      return render_template('home.html')
+        session['logged_in'] = True
     else:
-      return index()
+        flash('wrong password')
 
-@app.route("/template")
-def template():
-    return render_template('template.html')
+    return index()
+
+#@app.route("/template")
+#def template():
+#    return render_template('template.html')
+
+@app.route('/logout')
+def logout():
+    session['logged_inn'] = False
+    return render_template('login.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
 
 if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
     app.run(host='0.0.0.0',port=1111)
