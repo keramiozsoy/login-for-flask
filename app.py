e1ec35345e29e7b99b073ff9538a1f8ea0d54a7b
Kerami Ozsoy <keramiozsoy@hotmail.com>
2018-02-04 00:46:55 +0300

    Application can be use db now

--- app.py
+++ app.py
@@ -5,14 +5,21 @@ from flask import session
 from flask import flash
 import os
 
+from peewee import *
+
 app = Flask(__name__)
+
+db = SqliteDatabase('test.db')
+
 
 @app.route("/")
 def index():
+    records = Person.select()
+
     if not session.get('logged_in'):
         return render_template('login.html')
     else:
-        return render_template('home.html')
+        return render_template('home.html',result=records)
 
 @app.route("/login",methods=['POST'])
 def login():
@@ -35,6 +42,43 @@ def logout():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+class Person(Model):
+    name = CharField()
+    surname = CharField()
+    active = BooleanField()
+
+    class Meta:
+        database = db
+
+
+		
+class Pet(Model):
+    person = ForeignKeyField(Person, related_name='pets')
+    petName = CharField()
+
+    class Meta:
+        database = db
+
+db.connect()
+
+if not (
+	Person.table_exists() and Pet.table_exists()
+):
+	db.create_tables([Person,Pet])
+
+
+testData = Person(name='Kerami',surname='Ozsoy',active=True)
+testData.save()
+
+testData2 = Person(name='TestName',surname='TestSur',active=False)
+testData2.save()
+
+testData3 = Pet(person=testData,petName='kopek')
+testData3.save()
+
+db.close()
+
 
 if __name__ == "__main__":
     app.secret_key = os.urandom(12)
